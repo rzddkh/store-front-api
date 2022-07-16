c63261052e37658ceb0d27ae4e9c052907a64d7b
rzddkh <88165294+rzddkh@users.noreply.github.com>
2022-07-15 17:43:39 -0700

    adding testing database automatically

--- create_second_db.sh
+++ create_second_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "full_stack_user" --dbname "full_stack_dev" <<-EOSQL
     CREATE DATABASE full_stack_test;
 EOSQL
